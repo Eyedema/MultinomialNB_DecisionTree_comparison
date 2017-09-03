151209052c6fbdda59809a21ec462888f8f144f2
Ubaldo Puocci <u.puocci@gmail.com>
2017-09-03 11:35:19 +0200

    # of tries is now a parameter
    Signed-off-by: Ubaldo Puocci <u.puocci@gmail.com>

--- progetto.py
+++ progetto.py
@@ -18,16 +18,16 @@ from sklearn.tree import DecisionTreeClassifier
 from variables import MY_ID, MY_KEY
 
 
-def plot_learning_curve(X, y, title):
+def plot_learning_curve(X, y, tries, title):
+    global xx
     classifiers = [
         ("Decision Tree", DecisionTreeClassifier(), (0, 0.627, 0.690)),
         ("Naive Bayes", MultinomialNB(), (0.921, 0.407, 0.254)),
         #   max depth = 8
     ]
     #   debug save image
-    tries = 0
     for name, classifier, color in classifiers:
-        xx, yy, std_dev, yy_, std_dev_, tries = compute_scores(X, y, classifier)
+        xx, yy, std_dev, yy_, std_dev_ = compute_scores(X, y, tries, classifier)
         plt.plot(xx, yy, 'o-', label=name, color=color)
         plt.fill_between(xx, yy_ - std_dev_,
                          yy_ + std_dev_, alpha=0.2, color=color)
@@ -42,12 +42,11 @@ def plot_learning_curve(X, y, title):
 
 
 #   http://scikit-learn.org/stable/auto_examples/linear_model/plot_sgd_comparison.html#
-def compute_scores(X, y, classifier):
+def compute_scores(X, y, tries, classifier):
     heldout = [.9, .8, .7, .6, .5]
     xx = 1. - np.array(heldout)
     yy = []
     std_dev = []
-    tries = 10
     for i in xx:
         yy_ = []
         for r in range(1, tries + 1):
@@ -63,25 +62,25 @@ def compute_scores(X, y, classifier):
     #   Return also tries-1 for debug to save the image
     yy_ = np.array(yy)
     std_dev_ = np.array(std_dev)
-    return xx, yy, std_dev, yy_, std_dev_, tries
+    return xx, yy, std_dev, yy_, std_dev_
 
 
-def twenty_newsgroups():
+def twenty_newsgroups(tries):
     newsgroups = fetch_20newsgroups()
     X = TfidfTransformer().fit_transform(CountVectorizer().fit_transform(newsgroups.data))
     y = newsgroups.target
     title = "Learning Curve Comparison for 20newsgroups"
-    plot_learning_curve(X, y, title)
+    plot_learning_curve(X, y, tries, title)
 
 
-def loaddigits():
+def loaddigits(tries):
     digits = load_digits()
     X, y = digits.data, digits.target
     title = "Learning Curve Comparison for load_digits"
-    plot_learning_curve(X, y, title)
+    plot_learning_curve(X, y, tries, title)
 
 
-def mnist_data():
+def mnist_data(tries):
     mnist = fetch_mldata('MNIST original')
     X, y = mnist.data, mnist.target
     title = "Learning Curve Comparison for MNIST"
@@ -89,20 +88,20 @@ def mnist_data():
 
 
 #   https://stackoverflow.com/questions/1270951/how-to-refer-to-relative-paths-of-resources-when-working-with-a-code-repository
-def yahoo():
+def yahoo(tries):
     fn = os.path.join(os.path.dirname(__file__), "yahoo-web-directory-topics.libsvm")
     X, y = load_svmlight_file(fn)
     title = "Learning Curve Comparison for Yahoo"
     print X.shape, y.shape
-    plot_learning_curve(X, y, title)
+    plot_learning_curve(X, y, tries, title)
 
 
-def dmoz():
+def dmoz(tries):
     fn = os.path.join(os.path.dirname(__file__), "dmoz-web-directory-topics.libsvm")
     X, y = load_svmlight_file(fn)
     title = "Learning Curve Comparison for DMOZ"
     print X.shape, y.shape
-    plot_learning_curve(X, y, title)
+    plot_learning_curve(X, y, tries, title)
 
 
 def menu():
@@ -124,8 +123,11 @@ def exec_choice(choice):
         print "Error"
         return
     try:
+        print """How many tries would you
+                like to perform?"""
+        tries = input(">>")
         start_time = time.time()
-        actions[choice]()
+        actions[choice](tries)
     except KeyError:
         print "Invalid option"
         menu()
